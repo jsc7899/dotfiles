1481bf7b90a8943af78721d159599be666625e79
Jared Campbell <jcampbell7899@utexas.edu>
2024-07-02 16:24:41 -0500

    no swap - annoying

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -5,7 +5,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- disable swap
-vim.opt.swapfile = true
+vim.opt.swapfile = false
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
