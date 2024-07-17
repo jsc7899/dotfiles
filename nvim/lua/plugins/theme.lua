979baf995d8316191dc5e92f7d924a9864250a0e
Jared Campbell <jcampbell7899@utexas.edu>
2024-07-17 10:08:34 -0500

    use noir theme

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,11 +1,19 @@
 return {
-  'scottmckendry/cyberdream.nvim',
-  priority = 1000, -- Make sure to load this before all the other start plugins.
-  init = function()
-    -- Load the colorscheme here.
-    vim.cmd.colorscheme 'cyberdream'
+  -- 'scottmckendry/cyberdream.nvim',
+  -- priority = 1000, -- Make sure to load this before all the other start plugins.
+  -- italic_comments = true,
+  -- init = function()
+  --   -- Load the colorscheme here.
+  --   vim.cmd.colorscheme 'cyberdream'
+  --
+  --   -- You can configure highlights by doing something like:
+  --   vim.cmd.hi 'Comment gui=italic'
+  -- end,
 
-    -- You can configure highlights by doing something like:
-    vim.cmd.hi 'Comment gui=none'
+  -- https://github.com/andreasvc/vim-256noir
+  'andreasvc/vim-256noir',
+  config = function()
+    vim.cmd 'colorscheme 256_noir'
+    vim.cmd.hi 'Comment gui=italic'
   end,
 }
