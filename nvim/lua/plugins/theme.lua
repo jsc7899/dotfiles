fba044d5dd12311ce6d24fe239f871435a6499b6
Jared Campbell <jcampbell7899@utexas.edu>
2024-07-29 09:57:48 -0500

    back to cyberdream

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,19 +1,19 @@
 return {
-  -- 'scottmckendry/cyberdream.nvim',
-  -- priority = 1000, -- Make sure to load this before all the other start plugins.
-  -- italic_comments = true,
-  -- init = function()
-  --   -- Load the colorscheme here.
-  --   vim.cmd.colorscheme 'cyberdream'
-  --
-  --   -- You can configure highlights by doing something like:
-  --   vim.cmd.hi 'Comment gui=italic'
-  -- end,
+  'scottmckendry/cyberdream.nvim',
+  priority = 1000, -- Make sure to load this before all the other start plugins.
+  italic_comments = true,
+  init = function()
+    -- Load the colorscheme here.
+    vim.cmd.colorscheme 'cyberdream'
 
-  -- https://github.com/andreasvc/vim-256noir
-  'andreasvc/vim-256noir',
-  config = function()
-    vim.cmd 'colorscheme 256_noir'
+    -- You can configure highlights by doing something like:
     vim.cmd.hi 'Comment gui=italic'
   end,
+
+  -- https://github.com/andreasvc/vim-256noir
+  -- 'andreasvc/vim-256noir',
+  -- config = function()
+  --   vim.cmd 'colorscheme 256_noir'
+  --   vim.cmd.hi 'Comment gui=italic'
+  -- end,
 }
