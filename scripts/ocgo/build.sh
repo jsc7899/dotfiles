be05cb56bfc8f0eef083afabc26f8006ddced499
Jared Campbell <jcampbell7899@utexas.edu>
2024-10-30 16:37:50 -0500

    type

--- scripts/ocgo/build.sh
+++ scripts/ocgo/build.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 if git diff --name-only "$BITBUCKET_COMMIT_RANGE" | grep -q "^script/ocgo/"; then
     echo "Changes detected in script/ocgo/, proceeding with build..."
     # Build the package
-    cd script/ocgo || exit
+    cd scripts/ocgo || exit
     go build -o ocgo main.go
 else
     echo "No changes in script/ocgo/, skipping build step."
