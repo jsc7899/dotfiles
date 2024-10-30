7f08285e65e725b0a8db7525b51c129f883d2f53
Jared Campbell <jcampbell7899@utexas.edu>
2024-10-30 17:09:47 -0500

    more nomad/nmap funcs

---
+++ scripts/nomad/scav_ots_multi.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Path to the file containing the srcip and port pairs
+file="targets.txt"
+
+# Loop through each line of the file
+while read -r srcip port; do
+    ./scav_ots_manual.sh "$srcip" "$port"
+done <"$file"
