3b4c6341283b91ea71183502cd8f4826b3d511f2
Jared Campbell <jcampbell7899@utexas.edu>
2024-10-30 17:11:51 -0500

    config markdown lint and cleanup

--- nvim/lua/plugins/lint.lua
+++ nvim/lua/plugins/lint.lua
@@ -1,5 +1,5 @@
+local HOME = os.getenv 'HOME'
 return {
-
   { -- Linting
     'mfussenegger/nvim-lint',
     event = { 'BufReadPre', 'BufNewFile' },
@@ -7,48 +7,18 @@ return {
       local lint = require 'lint'
       lint.linters_by_ft = {
         bash = { 'shellcheck' },
-        -- ansible = { 'ansible_lint' },
-        --lua = { 'luacheck' },
         markdown = { 'markdownlint-cli2' },
+        -- ansible = { 'ansible_lint' },
+        -- lua = { 'luacheck' },
         -- python = { 'ruff' },
       }
-      -- require('lint').linters['markdownlint-cli2'].args = {
-      --   '--config',
-      --   '~/.config/.markdownlint-cli2.yaml',
-      -- }
-
-      -- To allow other plugins to add linters to require('lint').linters_by_ft,
-      -- instead set linters_by_ft like this:
-      -- lint.linters_by_ft = lint.linters_by_ft or {}
-      -- lint.linters_by_ft['markdown'] = { 'markdownlint' }
-      --
-      -- However, note that this will enable a set of default linters,
-      -- which will cause errors unless these tools are available:
-      -- {
-      --   clojure = { "clj-kondo" },
-      --   dockerfile = { "hadolint" },
-      --   inko = { "inko" },
-      --   janet = { "janet" },
-      --   json = { "jsonlint" },
-      --   markdown = { "vale" },
-      --   rst = { "vale" },
-      --   ruby = { "ruby" },
-      --   terraform = { "tflint" },
-      --   text = { "vale" }
-      -- }
-      --
-      -- You can disable the default linters by setting their filetypes to nil:
-      -- lint.linters_by_ft['clojure'] = nil
-      -- lint.linters_by_ft['dockerfile'] = nil
-      -- lint.linters_by_ft['inko'] = nil
-      -- lint.linters_by_ft['janet'] = nil
-      -- lint.linters_by_ft['json'] = nil
-      -- lint.linters_by_ft['markdown'] = nil
-      -- lint.linters_by_ft['rst'] = nil
-      -- lint.linters_by_ft['ruby'] = nil
-      -- lint.linters_by_ft['terraform'] = nil
-      -- lint.linters_by_ft['text'] = nil
-      -- lint.linters_by_ft['yaml'] = nil
+      local markdownlint_cli2 = require('lint').linters['markdownlint-cli2']
+      markdownlint_cli2.args = {
+        '--config',
+        -- '/opt/dotfiles/config/.markdownlint-cli2.yaml',
+        HOME .. '/dotfiles/config/.markdownlint-cli2.yaml',
+        '--',
+      }
       -- Create autocommand which carries out the actual linting
       -- on the specified events.
       local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
