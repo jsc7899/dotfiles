b78cc7411a7a48dd6d4bd69223741b2948952c89
Jared Campbell <jcampbell7899@utexas.edu>
2024-12-10 09:51:07 -0600

    reenable autocmds

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -13,29 +13,29 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 -- automatically determine filetype for ansible
--- vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
---   pattern = { '/opt/ansible*/*.yaml', '/Users/jared/.workstations/setup/*.yaml' },
---   command = 'set filetype=yaml.ansible',
--- })
---
--- -- Create an autocommand to adjust tab settings for yaml.ansible files
--- vim.api.nvim_create_autocmd({ 'BufEnter', 'FileType' }, {
---   pattern = { '*.yml', '*.yaml' },
---   callback = function()
---     if vim.bo.filetype == 'yaml.ansible' then
---       vim.bo.expandtab = true -- Use spaces instead of tabs
---       vim.bo.shiftwidth = 2 -- Size of an indent
---       vim.bo.tabstop = 2 -- Number of spaces tabs count for
---       vim.bo.softtabstop = 2 -- See tabstops at 2 spaces
---     end
---   end,
--- })
+vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+  pattern = { '/opt/ansible*/*.yaml' },
+  command = 'set filetype=yaml.ansible',
+})
+
+-- Create an autocommand to adjust tab settings for yaml.ansible files
+vim.api.nvim_create_autocmd({ 'BufEnter', 'FileType' }, {
+  pattern = { '*.yml', '*.yaml' },
+  callback = function()
+    if vim.bo.filetype == 'yaml.ansible' then
+      vim.bo.expandtab = true -- Use spaces instead of tabs
+      vim.bo.shiftwidth = 2 -- Size of an indent
+      vim.bo.tabstop = 2 -- Number of spaces tabs count for
+      vim.bo.softtabstop = 2 -- See tabstops at 2 spaces
+    end
+  end,
+})
 
 -- Enable spell checking for markdown and .md files
--- vim.api.nvim_create_autocmd({ 'FileType', 'BufRead', 'BufNewFile' }, {
---   pattern = { 'markdown', '*.md' },
---   callback = function()
---     vim.opt_local.spell = true
---     vim.opt_local.spelllang = { 'en_us' }
---   end,
--- })
+vim.api.nvim_create_autocmd({ 'FileType', 'BufRead', 'BufNewFile' }, {
+  pattern = { 'markdown', '*.md' },
+  callback = function()
+    vim.opt_local.spell = true
+    vim.opt_local.spelllang = { 'en_us' }
+  end,
+})
