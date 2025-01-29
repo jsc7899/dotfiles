dc0513bf45fbdfb6d9e0060bc3f3b8a8c42b9683
Jared Campbell <jcampbell7899@utexas.edu>
2025-01-28 18:56:12 -0600

    add podman pull and image reg pipelines

---
+++ scripts/pipelines.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# get the total size of all files older than 7d
+find . -type f -mtime +7 -exec ls -lh {} \; | tail -n +2 | awk '{print $5}' | numfmt --from=iec | awk '{sum+=1$1} END {print sum}' | numfmt --to=iec
+
+# pull all chomp podman images
+find /opt/chomp -name 'Dockerfile' | parallel 'podman pull void-registry.infosec.utexas.edu/utexasiso-$(basename $(dirname {})):production'
+
+# list all docker registry images
+curl -s https://void-registry.infosec.utexas.edu/v2/_catalog | jq '.repositories[]'
