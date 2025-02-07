d47a503168d8921651050b72c1e4e62dfb6fba06
Jared Campbell <jared@Jareds-MacBook-Air.local>
2025-02-06 23:15:56 -0600

    update config format

--- nvim/lua/plugins/autoformat.lua
+++ nvim/lua/plugins/autoformat.lua
@@ -1,44 +1,39 @@
--- Autoformat
 return {
   'stevearc/conform.nvim',
-  lazy = false,
-  keys = {
-    {
-      '<leader>af',
-      function()
-        require('conform').format { async = true, lsp_fallback = true }
-      end,
-      mode = '',
-      desc = '[F]ormat buffer',
-    },
-  },
-  opts = {
-    notify_on_error = false,
-    format_on_save = function(bufnr)
-      -- Disable "format_on_save lsp_fallback" for languages that don't
-      -- have a well standardized coding style. You can add additional
-      -- languages here or re-enable it for the disabled ones.
-      local disable_filetypes = { c = true, cpp = true }
-      return {
+  config = function()
+    require('conform').setup {
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        python = { 'ruff', 'ruff_format' }, -- Use ruff_format for Python
+        markdown = { 'markdownlint-cli2' },
+        bash = { 'beautysh' },
+        zsh = { 'beautysh' },
+        go = { 'gofmt' },
+      },
+      format_on_save = {
         timeout_ms = 500,
-        lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-      }
-    end,
-    formatters_by_ft = {
-      lua = { 'stylua' },
-      python = { 'ruff' },
-      markdown = { 'markdownlint-cli2' },
-      bash = { 'beautysh' },
-      zsh = { 'beautysh' },
-      go = { 'gofmt' },
-      -- yaml = { 'yamlfmt' },
-      -- ansible.yaml = {'yamlfmt'}
-      -- Conform can also run multiple formatters sequentially
-      -- python = { "isort", "black" },
-      --
-      -- You can use a sub-list to tell conform to run *until* a formatter
-      -- is found.
-      -- javascript = { { "prettierd", "prettier" } },
-    },
-  },
+        lsp_format = 'fallback',
+      },
+      formatters = {
+        ruff = {
+          command = 'ruff',
+          args = { 'check', '--select', 'I', '--fix', '--exit-zero', '-' },
+          stdin = true,
+        },
+      },
+    }
+
+    -- Define a user command for manual formatting
+    vim.api.nvim_create_user_command('Format', function(args)
+      local range = nil
+      if args.count ~= -1 then
+        local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+        range = {
+          start = { args.line1, 0 },
+          ['end'] = { args.line2, end_line:len() },
+        }
+      end
+      require('conform').format { async = true, lsp_format = 'fallback', range = range }
+    end, { range = true })
+  end,
 }
