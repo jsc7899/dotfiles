653841281bb75a76b3c305214343c11e028592c3
Jared Campbell <jcampbell7899@utexas.edu>
2025-03-02 22:18:23 -0600

    Enhance Telescope configuration: apply 'ivy' theme to 'find_files' and 'live_grep', and add new key mappings for finding Neovim, Chomp, Ansible, Dotfiles, and current directory files. Comment out diagnostics search functionality.

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -90,7 +90,7 @@ return {
           --height = 0.80,
           preview_cutoff = 100,
         },
-        file_ignore_patterns = { ".git/" },
+        file_ignore_patterns = { '.git/' },
         mappings = {
           i = { ['<c-enter>'] = 'to_fuzzy_refine' },
           -- ['<CR>'] = open_in_right_split
@@ -102,6 +102,10 @@ return {
         find_files = {
           follow = true,
           hidden = true,
+          theme = 'ivy',
+        },
+        live_grep = {
+          theme = 'ivy',
         },
         help_tags = { -- Customize behavior for 'help_tags' specifically
           attach_mappings = function(prompt_bufnr, map)
@@ -134,7 +138,7 @@ return {
     vim.keymap.set('n', '<leader>fs', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
     vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-    vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+    -- vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
     vim.keymap.set('n', '<leader>fr', builtin.resume, { desc = '[S]earch [R]esume' })
     vim.keymap.set('n', '<leader>f.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
     vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
@@ -158,8 +162,24 @@ return {
     end, { desc = '[S]earch [/] in Open Files' })
 
     -- Shortcut for searching your Neovim configuration files
-    vim.keymap.set('n', '<leader>sn', function()
+    vim.keymap.set('n', '<leader>fn', function()
       builtin.find_files { cwd = vim.fn.stdpath 'config' }
-    end, { desc = '[S]earch [N]eovim files' })
+    end, { desc = '[F]ind [N]eovim files' })
+
+    vim.keymap.set('n', '<leader>fc', function()
+      builtin.find_files { cwd = '/opt/chomp' }
+    end, { desc = '[F]ind [C]homp files' })
+
+    vim.keymap.set('n', '<leader>fa', function()
+      builtin.find_files { cwd = '/opt/ansible' }
+    end, { desc = '[F]ind [A]nsible files' })
+
+    vim.keymap.set('n', '<leader>fd', function()
+      builtin.find_files { cwd = vim.fn.stdpath 'config' }
+    end, { desc = '[F]ind [D]otfiles' })
+
+    vim.keymap.set('n', '<leader>sd', function()
+      builtin.find_files { cwd = vim.fn.expand '%:p:h' }
+    end, { desc = '[F]ind files in [D]irectory' })
   end,
 }
