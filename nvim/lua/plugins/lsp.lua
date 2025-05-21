101115869af3b2c639349e21c0cda553effb1901
Jared Campbell <jared@Jareds-MacBook-Air.local>
2025-05-20 23:28:19 -0500

    Remove extraneous blank line in LSP ensure_installed configuration for formatters list

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -262,7 +262,6 @@ return {
     -- for you, so that they are available from within Neovim.
     local ensure_installed = vim.tbl_keys(servers or {})
     vim.list_extend(ensure_installed, {
-
       -- formatters
       'stylua', -- Used to format Lua code
       'shfmt',
