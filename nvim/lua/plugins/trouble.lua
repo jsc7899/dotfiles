a3242f5e60f08514f035c10ad3605f50cff08924
Jared Campbell <jared@Jareds-MacBook-Air.local>
2025-05-20 23:28:25 -0500

    Update trouble.nvim config with refined keybindings and lazy loading on command; add descriptive annotations to key mappings

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -1,18 +1,51 @@
 -- https://github.com/folke/trouble.nvim
 return {
-	"folke/trouble.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons", "folke/todo-comments.nvim" },
-	opts = {
-		-- your configuration comes here
-		-- or leave it empty to use the default settings
-		-- refer to the configuration section below
-	},
-	keys = {
-		{ "<leader>tt", "<cmd>TroubleToggle<CR>" },
-		{ "<leader>tw", "<cmd>TroubleToggle workspace_diagnostics<CR>" },
-		{ "<leader>td", "<cmd>TroubleToggle document_diagnostics<CR>" },
-		{ "<leader>tq", "<cmd>TroubleToggle quickfix<CR>" },
-		{ "<leader>tl", "<cmd>TroubleToggle loclist<CR>" },
-		{ "<leader>to", "<cmd>ToDoTrouble<CR>" },
-	},
+  'folke/trouble.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons', 'folke/todo-comments.nvim' },
+  cmd = 'Trouble',
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  },
+  keys = {
+    {
+      '<leader>tt',
+      '<cmd>Trouble diagnostics toggle<cr>',
+      desc = 'Diagnostics (Trouble)',
+    },
+    {
+      '<leader>tT',
+      '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
+      desc = 'Buffer Diagnostics (Trouble)',
+    },
+    {
+      '<leader>cs',
+      '<cmd>Trouble symbols toggle focus=false<cr>',
+      desc = 'Symbols (Trouble)',
+    },
+    {
+      '<leader>cl',
+      '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
+      desc = 'LSP Definitions / references / ... (Trouble)',
+    },
+    {
+      '<leader>tL',
+      '<cmd>Trouble loclist toggle<cr>',
+      desc = 'Location List (Trouble)',
+    },
+    {
+      '<leader>tQ',
+      '<cmd>Trouble qflist toggle<cr>',
+      desc = 'Quickfix List (Trouble)',
+    },
+  },
+  -- keys = {
+  --   { '<leader>tt', '<cmd>TroubleToggle<CR>' },
+  --   { '<leader>tw', '<cmd>TroubleToggle workspace_diagnostics<CR>' },
+  --   { '<leader>td', '<cmd>TroubleToggle document_diagnostics<CR>' },
+  --   { '<leader>tq', '<cmd>TroubleToggle quickfix<CR>' },
+  --   { '<leader>tl', '<cmd>TroubleToggle loclist<CR>' },
+  --   { '<leader>to', '<cmd>ToDoTrouble<CR>' },
+  -- },
 }
