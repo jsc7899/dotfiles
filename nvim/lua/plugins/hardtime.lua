44038e79408afb7c5f4c63704166d70a6079e208
Jared Campbell <jcampbell7899@utexas.edu>
2025-05-26 14:56:46 -0500

    refactor: update 'hardtime' plugin configuration with new restrictions and settings

--- nvim/lua/plugins/hardtime.lua
+++ nvim/lua/plugins/hardtime.lua
@@ -3,7 +3,20 @@ return {
   lazy = false,
   dependencies = { 'MunifTanjim/nui.nvim' },
   opts = {
+    enabled = false,
     disable_mouse = false,
     restriction_mode = 'hint',
+    restricted_keys = {
+      ['h'] = { 'n', 'x' },
+      -- ['j'] = { 'n', 'x' },
+      -- ['k'] = { 'n', 'x' },
+      ['l'] = { 'n', 'x' },
+      ['+'] = { 'n', 'x' },
+      ['gj'] = { 'n', 'x' },
+      ['gk'] = { 'n', 'x' },
+      ['<C-M>'] = { 'n', 'x' },
+      ['<C-N>'] = { 'n', 'x' },
+      ['<C-P>'] = { 'n', 'x' },
+    },
   },
 }
